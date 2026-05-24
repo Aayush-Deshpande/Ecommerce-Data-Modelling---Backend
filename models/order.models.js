09a76f6f4add95bfff932d5611f53a7576640295
Aayush-Deshpande <aayushdeshpande532@gmail.com>
2026-05-24 19:27:23 +0530

    Built the entire Database model using mongoose

--- models/order.models.js
+++ models/order.models.js
@@ -1,7 +1,37 @@
 import mongoose from "mongoose"
 
-const orderSchema = new mongoose.Schema({
+const orderItemSchema = new.mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    } 
+})
 
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number, 
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    orderItems: {
+        type: [orderItemSchema]
+    }, 
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PENDING", "CANCELLED", "DELIVERED"],
+        default: "Pending"
+    }
 
 
 }, {timestamps: true})
